bb3114398f7246bb32bcf844b698b18a449e6e13
Dax1d <64999870+Dax1d@users.noreply.github.com>
2020-05-23 23:45:43 -0400

    Added website folder structure with html, css, and javascript

---
+++ root/js/DSC.js
@@ -0,0 +1,12 @@
+/**
+
+Experiments in energy/fluid motion based off this PDF 
+wyattflanders.com/MeAndMyNeighborhood.pdf
+
+Zero webgl boilerplate, its all handled by shader-doodle. 
+Using buffers to store feedback in rgb channels with a sd-node component.
+Also built in uniforms for mouse/drag start end
+
+https://github.com/halvves/shader-doodle
+
+*/
